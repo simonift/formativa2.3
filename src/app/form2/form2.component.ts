f45d792f03d881c30dfb3fefbb9d865cacaca80f
simonift <simon.faundezt@gmail.com>
2021-06-23 11:14:03 -0400

    Agregar bootstrap

--- src/app/form2/form2.component.ts
+++ src/app/form2/form2.component.ts
@@ -31,10 +31,10 @@ export class Form2Component {
 
     if (nota1>=4 && nota2>=4 && nota3>=4 && examen>=4){
       this.promedio=(((nota1+nota2+nota3)/3)*0.7)+(examen*0.3)
-      this.salida="Aprobado con nota: "+this.promedio;
+      this.salida="Aprobaste con nota "+this.promedio;
     } else {
       this.promedio=(((nota1+nota2+nota3)/3)*0.7)+(examen*0.3)
-      this.salida="Reprobado con nota: "+this.promedio;
+      this.salida="Reprobaste con nota "+this.promedio;
     }
   }
 
