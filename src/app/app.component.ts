083369a2feca10d91ce91f5aa1ee20422b3886d8
simonift <simon.faundezt@gmail.com>
2021-06-23 12:09:17 -0400

    Limpiando carpetas extras

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,47 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // title = 'unidad2';
-  // nombre = 'Simón Faúndez';
-  // edad = 25;
-  // email = 'simon.faundezt@gmail.com';
-  // sueldos = [1200, 1350, 1500];
-  // estado = true;
-
-  // notas = [50, 60, 70, 50, 60]
-  // actividad = ['supervisor', 'supervisor', 'encargado de obra', 'encargado de obra', 'encargado de obra']
-
-  // tieneTrabajo(){
-  //   if(this.estado){
-  //     return 'Tiene trabajo';
-  //   } else {
-  //     return 'No tiene trabajo';
-  //   }
-  // }
-
-  // sumarSueldos(){
-  //   let suma = 0
-  //   for (let i=0; i<this.sueldos.length; i++){
-  //     suma = suma + this.sueldos[i]
-  //   }
-  //   return suma
-  // }
-
-  // calcularPromedio(){
-  //   let suma=0
-  //   let promedio=0
-  //   for(let i=0; i<this.notas.length; i++){
-  //     suma = suma + this.notas[i]
-  //   }
-  //   promedio = suma/this.notas.length
-  //   return promedio
-  // }
-
-  // elementos = [{sueldos:1200, notas:60, actividad:'supervisor'}, {sueldos:1500, notas:50, actividad:'supervisor'},
-  // {sueldos:1350, notas:70, actividad:'encargado de obra'},
-  // {sueldos:1400, notas:60, actividad:'encargado de obra'}];
-
-
-
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,11 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EventosComponent } from './eventos/eventos.component';
-import { Form1Component } from './form1/form1.component';
-import { AzarComponent } from './azar/azar.component';
-import { EnlacePropiedadesComponent } from './enlace-propiedades/enlace-propiedades.component';
-import { OpcionesComponent } from './opciones/opciones.component';
-import { Formulario1Component } from './formulario1/formulario1.component';
 import { Form2Component } from './form2/form2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EventosComponent,
-    Form1Component,
-    AzarComponent,
-    EnlacePropiedadesComponent,
-    OpcionesComponent,
-    Formulario1Component,
     Form2Component
   ],
   imports: [
